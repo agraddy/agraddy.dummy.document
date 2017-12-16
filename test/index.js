66beb927c334431a354c5edffc60c9946bd8bded
Anthony Graddy <anthony@locationq.com>
2017-12-16 20:48:06 +0000

    Initial code

--- test/index.js
+++ test/index.js
@@ -1,8 +1,15 @@
-process.chdir('test');
+var path = require('path');
+process.chdir(path.dirname(__filename));
 var tap = require('agraddy.test.tap')(__filename);
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+var document = mod;
+
+tap.assert.equal(typeof document, 'object', 'Should be an object.');
+
+tap.assert.equal(typeof document.querySelector, 'function', 'querySelector.');
+
+tap.assert.equal(typeof document.querySelector().addEventListener, 'function', 'addEventListener');
 
 
